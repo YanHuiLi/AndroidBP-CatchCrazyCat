9fcfc2c5777ed63a961e05ec8e834647b27270f6
Archer <421228286@qq.com>
2016-05-24 17:32:58 +0800

    完成简单的点移动策略

--- app/src/main/java/com/example/archer/catchcrazycat/Playgroud.java
+++ app/src/main/java/com/example/archer/catchcrazycat/Playgroud.java
@@ -5,22 +5,27 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Vector;
 
 /**
  * Created by Archer on 2016/5/24.
  */
 
 //绘制界面
-public class Playgroud extends SurfaceView {
+public class Playgroud extends SurfaceView implements View.OnTouchListener {
 
-    private static    int WIDTH=72;
-    private static  final  int ROW=10;
-    private static  final  int COL=10;
+    private static int WIDTH = 72;
+    private static final int ROW = 10;
+    private static final int COL = 10;
     //设置路障初始化为10
-    private static  final  int BLOCK=15;
-    private   Dot matrix[][];
+    private static final int BLOCK = 15;
+    private Dot matrix[][];
     private Dot cat;
 
 
@@ -28,23 +33,151 @@ public class Playgroud extends SurfaceView {
         super(context);
 
         getHolder().addCallback(callback);
-        matrix=new Dot[ROW][COL];
+        matrix = new Dot[ROW][COL];
 
-        for (int i =0;i<ROW;i++){
-            for (int j=0;j<COL;j++){
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
                 //恰恰相反，注意
-                matrix[i][j]=new Dot(j,i);
+                matrix[i][j] = new Dot(j, i);
             }
         }
 
         //点创建完成以后调用
-            initGame();
+        initGame();
+        setOnTouchListener(this);
     }
 
-    private Dot getDot(int x, int y){
+    private Dot getDot(int x, int y) {
 
         return matrix[y][x];
     }
+
+    private boolean isAtEge(Dot dot) {
+
+        //判断一个点是否是边界
+
+        if (dot.getX() * dot.getY() == 0 || dot.getX() + 1 == COL || dot.getY() + 1 == ROW) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    //得到周围的点，平行左边第一个点,顺时针
+    private Dot getNrighbour(Dot one, int dir) {
+
+        switch (dir) {
+            case 1:
+                return getDot(one.getX() - 1, one.getY());
+
+            case 2:
+
+                if (one.getY() % 2 == 0) {
+                    return getDot(one.getX() - 1, one.getY() - 1);
+                } else {
+                    return getDot(one.getX(), one.getY() - 1);
+                }
+
+            case 3:
+                if (one.getY() % 2 == 0) {
+                    return getDot(one.getX(), one.getY() - 1);
+
+                } else {
+                    return getDot(one.getX() + 1, one.getY() - 1);
+                }
+
+            case 4:
+                return getDot(one.getX() + 1, one.getY());
+
+
+            case 5:
+                if (one.getY() % 2 == 0) {
+                    return getDot(one.getX(), one.getY() + 1);
+
+                } else {
+                    return getDot(one.getX() + 1, one.getY() + 1);
+                }
+
+            case 6:
+                if (one.getY() % 2 == 0) {
+
+                    return getDot(one.getX() - 1, one.getY() + 1);
+                } else {
+                    return getDot(one.getX(), one.getY() + 1);
+                }
+            default:
+                break;
+        }
+        return null;
+    }
+
+
+private int getDistance(Dot one,int dir){
+
+    int distance=0;
+
+    Dot ori=one,next;
+
+    while(true){
+
+        next=getNrighbour(ori,dir);
+        //遇到路障
+        if (next.getStatus()==Dot.STATIC_ON){
+            return distance*-1;
+        }
+        if (isAtEge(next)){
+            distance++;
+            return  distance;
+        }
+        distance++;
+        ori=next;
+    }
+
+
+}
+
+    private void MoveTo(Dot one){
+
+        one.setStatus(Dot.STATIC_IN);
+        getDot(cat.getX(),cat.getY()).setStatus(Dot.STATIC_OFF);
+        cat.setXY(one.getX(),one.getY());
+
+    }
+
+    private void  move(){
+        if (isAtEge(cat)){
+            lose();
+            return;
+        }
+        Vector<Dot> avaliable=new Vector<>();
+        for (int i=1;i<7;i++){
+           Dot n= getNrighbour(cat,i);
+            if (n.getStatus()==Dot.STATIC_OFF){
+                avaliable.add(n);
+            }
+
+        }
+        if (avaliable.size()==0){
+            win();
+        }else {
+            MoveTo(avaliable.get(0));
+        }
+    }
+
+    private void lose(){
+
+        Toast.makeText(getContext(),"Lose the game",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void win(){
+
+        Toast.makeText(getContext(),"Win the game",Toast.LENGTH_LONG).show();
+
+    }
+
+
 
     private void redraw() {
         Canvas canvas = getHolder().lockCanvas();
@@ -63,11 +196,12 @@ public class Playgroud extends SurfaceView {
                 offset=WIDTH/2;
             }
             for (int j=0; j<COL;j++){
-              Dot one=  getDot(j,i);
+                Dot one=  getDot(j,i);
                 switch (one.getStatus()){
                     case Dot.STATIC_OFF:
 
                         paint.setColor(0xFFEEEEEE);
+
                         break;
                     case Dot.STATIC_IN:
 
@@ -94,7 +228,7 @@ public class Playgroud extends SurfaceView {
 
         @Override
         public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                    WIDTH=width/(COL+1);
+            WIDTH=width/(COL+1);
             redraw();
         }
 
@@ -104,7 +238,7 @@ public class Playgroud extends SurfaceView {
         }
     };
 
-//游戏初始化
+    //游戏初始化
     private void initGame(){
         for (int i=0;i<ROW;i++){
             for (int  j=0;j<COL;j++){
@@ -123,5 +257,45 @@ public class Playgroud extends SurfaceView {
                 System.out.println("Block"+i);
             }
         }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if (event.getAction()==MotionEvent.ACTION_UP){
+//            Toast.makeText(getContext(), event.getX()+" "+event.getY(),Toast.LENGTH_LONG).show();
+            int x,y;
+
+            y= (int) (event.getY()/WIDTH);
+
+            if (y%2==0){
+                x= (int) (event.getX()/WIDTH);
+            }else {
+//偏移了半个元素的宽度
+                x= (int) ((event.getX()-WIDTH/2)/WIDTH);
+                //路障模式
+
+            }
+            if (x+1>COL||y+1>ROW){
+
+                initGame();
+
+
+            }else if ( getDot(x,y).getStatus()==Dot.STATIC_OFF){
+
+
+                getDot(x,y).setStatus(Dot.STATIC_ON);
+
+                move();
+
+            }
+
+            redraw();
+        }
+
+
+
+
+        return true;
     }
 }
